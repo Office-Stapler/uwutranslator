6d5777c6d096108fa6f6ddfe1f67e6d9c1b65356
Raymond Lam <53181007+Office-Stapler@users.noreply.github.com>
2019-07-25 18:14:39 +1000

    Update uwutranslator.c

--- uwutranslator.c
+++ uwutranslator.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define KNRM  "\x1B[0m"
-#define KRED  "\x1B[31m"
-#define KGRN  "\x1B[32m"
-#define KYEL  "\x1B[33m"
-#define KBLU  "\x1B[34m"
-#define KMAG  "\x1B[35m"
-#define KCYN  "\x1B[36m"
-#define KWHT  "\x1B[37m"
+#define NRM  "\x1B[0m"
+#define RED  "\x1B[31m"
+#define GRN  "\x1B[32m"
+#define YEL  "\x1B[33m"
+#define BLU  "\x1B[34m"
+#define MAG  "\x1B[35m"
+#define CYN  "\x1B[36m"
+#define WHT  "\x1B[37m"
 
 int main(int argc, char* argv[]) {
     FILE *f = fopen(argv[1], "r");
@@ -18,15 +18,18 @@ int main(int argc, char* argv[]) {
         switch(c) {
             case 'r' :
             case 'l' :
+				printf(GRN);
                 c = 'w';
                 break;
             case 'R' :
             case 'L' :
-                c = 'R';
+				printf(GRN);
+                c = 'W';
                 break;
             case 't' :
                 check = fgetc(f);
                 if (check == 'H' || check == 'h') {
+					printf(GRN);
                     c = 'd';
                 } else {
                     check1 = check;
@@ -38,6 +41,7 @@ int main(int argc, char* argv[]) {
             case 'T' :
                 check = fgetc(f);
                 if (check == 'H' || check =='h') {
+					printf(GRN);
                     c = 'D';
                 } else {
                     check1 = check;
@@ -49,6 +53,7 @@ int main(int argc, char* argv[]) {
                 break;
         }
         fputc(c, stdout);
+		printf(WHT);
         c = fgetc(f);
     }
     printf("\n");
